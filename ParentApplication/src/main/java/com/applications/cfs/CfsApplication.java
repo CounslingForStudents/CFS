068b23a51ae5fa839d8195cc2ee75bb15fb0a07a
Bojja Srikar <NHI136@nslhub.in>
2023-05-29 17:15:46 +0530

    first commit

---
+++ ParentApplication/src/main/java/com/applications/cfs/CfsApplication.java
@@ -0,0 +1,12 @@
+package com.applications.cfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class CfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfsApplication.class, args);
+	}
+
+}
